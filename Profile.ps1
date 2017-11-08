8ce2941d4dd28a830d0113d07bc7c9408ab17bcf
Simon Randby <simon.randby@gmail.com>
2017-11-08 21:18:08 +0100

    Add touch
    Was annoying to expect it to be there and then suddenly remembering.
    Now it is there.

--- Profile.ps1
+++ Profile.ps1
@@ -12,5 +12,21 @@ function which($name)
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
+function touch($file)
+{
+    if($file -eq $null)
+    {
+        throw "Touch requires that you submit a file to create or update"
+    }
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
+
 # Uncomment to override path with user-defined string:
 # $env:Path = ((Get-Content $HOME/Documents/WindowsPowerShell/path.txt -Raw) -replace "(?m)#.*`n?", '').Replace("`r`n","")
