98ad9498daf9cc4a7b1b9b1aba2ef9499869e72c
Simon Randby <simon.randby@gmail.com>
2018-09-02 01:28:20 +0200

    Add "sudo" for powershell
    A primitive take on it really, but beats having to wait for the start
    menu search to figure out what the meaning of life truely is.

--- WindowsPowerShell/Modules/system-utils/system-utils.psm1
+++ WindowsPowerShell/Modules/system-utils/system-utils.psm1
@@ -9,4 +9,12 @@ function Restart-Explorer
     Stop-Process -ProcessName explorer
 }
 
+function sudo
+{
+    # This should maybe try to take the arguments, and then pass them to be run in the new elevated shell that is spawned.
+    # Or maybe start the new elevated session in the same directory which it is called from.
+    # For now it is a weak reproduction of `sudo -i`/`sudo su`, but still (slightly) better than the alternatives.
+    Start-Process powershell -Verb RunAs
+}
+
 Export-ModuleMember -Function *
