8a95089fcb56be4e1fba1a9bdcd49b0b2fbd5a01
Simon Randby <simon.randby@gmail.com>
2020-12-15 15:53:26 +0100

    PS Text encoding module: Add support for args from pipeline

--- PowerShell/Modules/text-encoding/text-encoding.psm1
+++ PowerShell/Modules/text-encoding/text-encoding.psm1
@@ -1,32 +1,44 @@
-function base64Encode ([string]$text)
+function base64Encode ([parameter(ValueFromPipeline=$True)][string]$text)
 {
-    $bytes = [Text.Encoding]::UTF8.GetBytes($text)
-    $encodedText = [Convert]::ToBase64String($bytes)
-    return $encodedText
-}
-
-function base64Decode ([string]$encodedText)
-{
-    $extraPaddingCharactersNeeded = (4 - ($encodedText.Length % 4)) % 4
-    $padding = ""
-    if($extraPaddingCharactersNeeded -ne 0)
+    Process
     {
-        $padding = '=' * $extraPaddingCharactersNeeded
+        $bytes = [Text.Encoding]::UTF8.GetBytes($text)
+        $encodedText = [Convert]::ToBase64String($bytes)
+        return $encodedText
     }
-    $paddedText = $encodedText + $padding
-    $bytes = [System.Convert]::FromBase64String($paddedText)
-    $decodedText = [System.Text.Encoding]::UTF8.GetString($bytes)
-    return $decodedText
 }
 
-function urlEncode ([string]$text)
+function base64Decode ([parameter(ValueFromPipeline=$True)][string]$encodedText)
 {
-    return [System.Web.HttpUtility]::UrlEncode($text)
+    Process
+    {
+        $extraPaddingCharactersNeeded = (4 - ($encodedText.Length % 4)) % 4
+        $padding = ""
+        if($extraPaddingCharactersNeeded -ne 0)
+        {
+            $padding = '=' * $extraPaddingCharactersNeeded
+        }
+        $paddedText = $encodedText + $padding
+        $bytes = [System.Convert]::FromBase64String($paddedText)
+        $decodedText = [System.Text.Encoding]::UTF8.GetString($bytes)
+        return $decodedText
+    }
 }
 
-function urlDecode ([string]$encodedText)
+function urlEncode ([parameter(ValueFromPipeline=$True)][string]$text)
 {
-    return [System.Web.HttpUtility]::UrlDecode($encodedText)
+    Process
+    {
+        return [System.Web.HttpUtility]::UrlEncode($text)
+    }
+}
+
+function urlDecode ([parameter(ValueFromPipeline=$True)][string]$encodedText)
+{
+    Process
+    {
+        return [System.Web.HttpUtility]::UrlDecode($encodedText)
+    }
 }
 
 Export-ModuleMember -Function *
